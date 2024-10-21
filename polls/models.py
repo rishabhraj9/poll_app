266868e9f4cdf978489df174247e4d35ea434c2e
rishabhraj9 <rishabhraj426@gmail.com>
2024-10-21 17:27:36 -0400

    fixes black

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,6 @@ import datetime
 
 
 class Question(models.Model):
-
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
@@ -16,7 +15,6 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
